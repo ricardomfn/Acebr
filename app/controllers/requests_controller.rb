3e1cc9ee9db756abc4a2c932b81affe3e985500c
ricardo martins <ricardomfneves@gmail.com>
2023-06-28 12:25:55 +0200

    ok

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -22,7 +22,6 @@ class RequestsController < ApplicationController
 
   def create
     @match = Match.find(params[:match_id])
-    raise
     @request = Request.new
     @request.match = @match
     @request.user = current_user
